d5e8b7373943d6cebc071077d8a952e8a0b63746
zhang30 <zhang30@scotty>
2012-08-10 10:22:00 -0700

    Create a apps subdirectory to store the application programs

---
+++ apps/mmpbsa/amberTools.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   amberTools.cpp
+ * Author: zhang30
+ *
+ * Created on August 10, 2012, 10:15 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
