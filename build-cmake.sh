ec0570b956d42d3f4a2d1c8e2d4e49a1de18d652
Xiaohua Zhang <zhang30@llnl.gov>
2017-06-30 16:49:27 -0700

    Make install works for the code

--- build-cmake.sh
+++ build-cmake.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-export BOOST_ROOT=/usr/gapps/bbs/jade/
 
-ccmake ../ -DCMAKE_INSTALL_PREFIX=/usr/gapps/kras/quartz/medcm -DBoost_DIR=/usr/gapps/bbs/jade/ -DBoost_INCLUDE_DIR=/usr/gapps/bbs/jade/include
+cmake ../ -DCMAKE_INSTALL_PREFIX=/usr/gapps/kras/quartz/medcm -DBOOST_ROOT=/usr/gapps/bbs/jade/ 
